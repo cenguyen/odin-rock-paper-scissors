80b5af165ea028d9ee0cb06c065efbb08b8dd7ce
cenguyen <cenguyen9@gmail.com>
2022-02-04 16:36:54 -0600

    added jankenpon function with just player chooses rock outcomes

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function computerPlay() {
 
 console.log(computerPlay());
 
+function jankenPon(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection;
+    if (playerChoice === "rock" && computerChoice === "rock") {
+        return "Draw! Try again!";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        return "You lose! Paper beats rock!";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "You win! Rock beats scissors!";
+    }
+}
+
+let playerSelection = "rock";
+let computerChoice = computerPlay();
+console.log(jankenPon(playerSelection, computerChoice));
